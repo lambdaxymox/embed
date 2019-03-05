782be9916046544024a7b6dd11ba35c9cf3e64fd
LambdaXymox <lambda.xymox@gmail.com>
2019-03-04 19:35:25 -0600

    Add embed macro.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_export]
+macro_rules! embed {
+    ($asset:expr) => {
+        let bytes_arr = include_bytes!(asset);
+        let mut bytes_vec: Vec<u8> = vec![0; &bytes_arr[..].len()];
+        for i in 0..bytes_vec.len() {
+            bytes_vec[i] = bytes_arr[i];
+        }
+
+        bytes_vec
     }
 }
