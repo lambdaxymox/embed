aabc6e42a382d890033acbcd8c8bfc18a38f6814
LambdaXymox <lambda.xymox@gmail.com>
2019-03-04 22:10:51 -0600

    Move array conversion code to a separate function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
+#[inline]
+fn __arr_to_vec(bytes: &[u8]) -> Vec<u8> {
+    let length = bytes.len();
+    let mut vec = vec![0 as u8; length];
+    for i in 0..length {
+        vec[i] = bytes[i];
+    }
+
+    vec
+}
+
+
 #[macro_export]
 macro_rules! embed {
-    ($asset:expr) => {
+    ($asset:expr) => {{
         let bytes_arr = include_bytes!(asset);
-        let mut bytes_vec: Vec<u8> = vec![0; &bytes_arr[..].len()];
-        for i in 0..bytes_vec.len() {
-            bytes_vec[i] = bytes_arr[i];
-        }
-
-        bytes_vec
-    }
+        __arr_to_vec(&bytes_arr[..])
+    }}
 }
