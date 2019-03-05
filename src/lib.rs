1b1147fdb0af263b7653c8a86763f1fae67f126c
LambdaXymox <lambda.xymox@gmail.com>
2019-03-04 22:38:00 -0600

    Add more documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! # Embed
+//! This repository exports a macro for embedding static art assets into Rust binaries.
+//! 
+//! ## Usage
+//! To use the embed macro in your application, add the following line to your Rust dependencies.
+//! ```
+//! embed = { git = "https://github.com/stallmanifold/embed" }
+//! ```
+//! 
+//! ## Examples
+//! Here is an example of using the `embed` macro in a working program.
+//! ```rust
+//! use embed::embed;
+//! 
+//! fn main() {
+//!     let obj = embed!("../assets/triangle.obj");
+//!     println!("{:?}", obj);
+//! }
+//! ```
+//! 
+
+/// The embed macro takes a file path, statically loads the file, and generates the code 
+/// to drop it into a binary vector at runtime. 
 #[macro_export]
 macro_rules! embed {
     ($asset:expr) => {{
